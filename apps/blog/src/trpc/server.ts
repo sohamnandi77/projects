12bec66f35bdbb91affdce23659b66c88c3a418e
Soham Nandi <sohamnandi77@gmail.com>
2024-12-06 03:58:22 +0530

    fix(trpc): remove caching from createContext and getQueryClient for improved clarity and performance

--- apps/blog/src/trpc/server.ts
+++ apps/blog/src/trpc/server.ts
@@ -1,4 +1,3 @@
-import { cache } from "react";
 import { headers } from "next/headers";
 import { createHydrationHelpers } from "@trpc/react-query/rsc";
 
@@ -12,7 +11,7 @@ import { createQueryClient } from "./query-client";
  * This wraps the `createTRPCContext` helper and provides the required context for the tRPC API when
  * handling a tRPC call from a React Server Component.
  */
-const createContext = cache(async () => {
+const createContext = async () => {
   const heads = new Headers(await headers());
   heads.set("x-trpc-source", "rsc");
 
@@ -20,9 +19,9 @@ const createContext = cache(async () => {
     session: await getSession(),
     headers: heads,
   });
-});
+};
 
-const getQueryClient = cache(createQueryClient);
+const getQueryClient = createQueryClient;
 const caller = createCaller(createContext);
 
 export const { trpc: api, HydrateClient } = createHydrationHelpers<AppRouter>(
